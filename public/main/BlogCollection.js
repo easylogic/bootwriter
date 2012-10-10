f913de3ec1a2e9094c435812951db88f577e9046
easylogic <cyberuls@gmail.com>
2012-10-10 14:09:37 +0900

    only client javascript app

--- public/main/BlogCollection.js
+++
@@ -1,6 +0,0 @@
-define(['./BlogItemModel'], function(model) { 
-    return Backbone.Collection.extend({
-        model : model,
-        url : "/toys"
-    });    
-})
--- public/main/app.js
+++
@@ -1,8 +0,0 @@
-define(['./BlogMain', './BlogMenu'], function(BlogMain, BlogMenu){
-    return { 
-        initialize: function() { 
-             var main = new BlogMain();
-             $('#contents').append(main.render().el);
-        }        
-    };
-})
